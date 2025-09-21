e64c9378b130d2f3c163c994e34763b7f40a1d50
jsschuler <jsschuler@gmail.com>
2025-09-20 21:09:34 -0400

    modified:   modelRunFunctions.jl

--- modelRunFunctions.jl
+++ modelRunFunctions.jl
@@ -3,8 +3,8 @@
     # 1 Sold houses become populated houses and their owners move to hotels
     # 2 Sold Exit houses become populated houses and their owners leave the market
     # 3. a number of new agents enter the market and dwell in hotels
-    # 4. a number of agents decide to leave the market and list their houses as exit houses
-    # 5. a number of new houses are built 
+    # 4. a number of new houses are built 
+    # 5. a number of agents decide to leave the market and list their houses as exit houses
     # 6. a number of agents decide to move within the market and list their houses as for sale houses
     # 7 now, the inner loop starts and runs for a set number of rounds
         # a preference graph is generated connecting hotels to empty houses, exit houses, or for sale houses
@@ -46,5 +46,39 @@ function upForSale!(env::environment)
         else
             exit!(env,hausOrder[i])
         end
+    end
+end
+
+function construct!(env::environment)
+    newHaus=emptyHouse(length(env.allHouses)+1,rand(env.qualityDistribution,1)[1])
+    push!(env.allHouses,newHaus)
+    push!(env.emptyHouses,newHause)
+    return newHaus
+end
+
+function allConstruct!(env::environment)
+    for i in 1:env.construction
+        construct!(env)
+    end
+end
+# now we need functions that build the dictionaries that support the network
+function dictGen!(env::environment)
+    env.intDict=Dict{Int64,dwelling}{}
+    env.nodeDict=Dict{dwelling,Int64}{}
+    for haus in env.forSaleHouses
+        env.intDict[haus.idx]=haus
+        env.nodeDict[haus]=haus.idx
+    end
+    for haus in env.emptyHouses
+        env.intDict[haus.idx]=haus
+        env.nodeDict[haus]=haus.idx
+    end
+    for haus in env.exitHouses
+        env.intDict[haus.idx]=haus
+        env.nodeDict[haus]=haus.idx
+    end
+    for hot in env.allHotels
+        env.intDict[hot.idx]=hot
+        env.nodeDict[hot]=hot.idx
     end
 end
